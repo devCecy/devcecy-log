cd700f12693d04895c11bb848ea9e8b9df15094e
devCecy <im.ceciliaan@gmail.com>
2023-02-14 22:33:02 +0900

    fix : 애널리틱스 스크립트 app파일로 이동

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,13 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
+// import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { pageview } from '@/components/analytics/GoogleAnalytics'
+import Script from 'next/script'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -38,7 +39,25 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
+      {/* <Analytics /> */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
