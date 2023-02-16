e96e2f7678d7b81af55a5803349626415c335bfa
devCecy <im.ceciliaan@gmail.com>
2023-02-16 09:22:52 +0900

    feat : ga 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
 import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-
-import siteMetadata from '@/data/siteMetadata'
-// import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { pageview } from '@/components/analytics/GoogleAnalytics'
 import Script from 'next/script'
+
+import siteMetadata from '@/data/siteMetadata'
+import { ClientReload } from '@/components/ClientReload'
+import LayoutWrapper from '@/components/LayoutWrapper'
+import { pageview } from '@/components/analytics/GoogleAnalytics'
+// import Analytics from '@/components/analytics'
+// import GoogleAnalytics from '@/components/analytics/GoogleAnalytics'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -39,7 +39,7 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      {/* <Analytics /> */}
+      {/* <GoogleAnalytics /> */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
