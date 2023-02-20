cb181893d704d8cd08608e0edd44851597ab4308
devCecy <im.ceciliaan@gmail.com>
2023-02-20 18:47:25 +0900

    fix : ga수정

--- pages/_app.js
+++ pages/_app.js
@@ -40,8 +40,16 @@ export default function App({ Component, pageProps }) {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
-
       <Script strategy="afterInteractive" id="ga-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+
+        {/* <Script strategy="afterInteractive" id="ga-script">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -49,7 +57,7 @@ export default function App({ Component, pageProps }) {
             gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
               page_path: window.location.pathname,
             });
-        `}
+        `} */}
       </Script>
       <LayoutWrapper>
         <Component {...pageProps} />
